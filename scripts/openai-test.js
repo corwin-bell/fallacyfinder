30d7b49f274510873f8d45f6e1e9777034e2f935
corwin-bell <corwin.m.bell@gmail.com>
2024-03-20 14:30:17 -0600

    integration of OpenAI API successful

--- scripts/openai-test.js
+++ scripts/openai-test.js
@@ -1,11 +1,9 @@
+// import OpenAI from "openai"; this doesn't work unless the file is a module
+// using require instead
 const OpenAI = require("openai");
 const openai = new OpenAI();
 
 async function main() {
-    // add event listener for when user clicks on extension
-    // for each paragraph in web page
-        // concatenate as one string
-    // pass the concatenated string to chat completions function
     const text = `The University of Mississippi recently passed a policy banning smoking on campus. 
     I am a smoker, and I have a lot of friends who are smokers, and we all agree that this policy should be overturned. 
     This policy is framed in terms of health outcomes and promoting individual well-being, but the University has not instituted policies regarding many other behaviors related to health, such as exercising. 
@@ -15,7 +13,7 @@ async function main() {
     The policy also states, “All members of the university community share in the responsibility for adhering to and enforcing this policy.” That type of language asks students and faculty to be informants against each other.
     A college campus is a place for free expression of ideas and behaviors. It’s simple: either we are a freedom-loving campus or we aren’t. 
     I choose freedom, and I believe all of America’s veterans would agree with me. Perhaps the Chancellor has an ulterior motive for instituting the ban and is using smokers as a scapegoat.`
-    
+
     const completion = await openai.chat.completions.create({
         messages: [
             { 
@@ -31,7 +29,7 @@ async function main() {
         temperature: 1
     });
 
-    console.log(completion.choices[0].message.content);
+  console.log(completion.choices[0].message.content);
 }
 
 main();
